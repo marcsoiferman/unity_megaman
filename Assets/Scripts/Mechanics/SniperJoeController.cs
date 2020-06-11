a4a7aeff70dbfb69e5efd6957b465eb4282f2957
Jordan Reardon-Smith <49737691+HifenX10@users.noreply.github.com>
2020-06-10 21:39:50 -0500

    missing files

---
+++ Assets/Scripts/Mechanics/SniperJoeController.cs
@@ -0,0 +1,27 @@
+﻿using Platformer.Gameplay;
+using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs.LowLevel.Unsafe;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using Assets.Scripts;
+using Assets.Scripts.Gameplay;
+using Platformer.Gameplay;
+
+public class SniperJoeController : EnemyController, IEnemy
+{
+    public override float BounceAmount => 3;
+    public override bool HurtByJump => false;
+    public override int ContactDammage => 3;
+
+    Weapon weapon;
+    protected override void Awake()
+    {
+        base.Awake();
+        //_collider = GetComponents<Collider2D>()[1];
+        weapon = GetComponent<Weapon>();
+    }
+    // Start is called before the first frame update
+    void Start() { }
+}
